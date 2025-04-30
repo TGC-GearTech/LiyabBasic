b090266b6c6db6bef72227d661095f0e977c8dd8
TGC-GearTech <thegearconcept@gmail.com>
2025-05-01 00:44:44 +0800

    Update LiyabBasic.h

--- src/LiyabBasic.h
+++ src/LiyabBasic.h
@@ -10,6 +10,12 @@
 void Liyab_Init();
 void OK();
 void motor(uint8_t pin, int speed);
+void forward(int speedL, int speedR);
+void backward(int speedL, int speedR);
+void spin_left(int speedL, int speedR);
+void spin_right(int speedL, int speedR);
+void turn_right(int speed);
+void turn_left(int speed);
 void motor_stop();
 
 #endif 
