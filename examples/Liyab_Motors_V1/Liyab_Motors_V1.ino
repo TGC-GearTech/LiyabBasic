e3f819474f15ff149af73931d802d5ad8f6e58ac
TGC-GearTech <thegearconcept@gmail.com>
2025-05-06 18:17:45 +0800

    Update Liyab_Motors_V1.ino

--- examples/Liyab_Motors_V1/Liyab_Motors_V1.ino
+++ examples/Liyab_Motors_V1/Liyab_Motors_V1.ino
@@ -8,23 +8,23 @@ void setup() {
 }
 
 void loop() {
-motor(1, 50);
-motor(2, 50);
-delay(1000);
+  motor(1, 50);
+  motor(2, 50);
+  delay(1000);
 
-motor(1, -50);
-motor(2, -50);
-delay(1000);
+  motor(1, -50);
+  motor(2, -50);
+  delay(1000);
 
-motor(1, 50);
-motor(2, -50);
-delay(1000);
+  motor(1, 50);
+  motor(2, -50);
+  delay(1000);
 
-motor(1, -50);
-motor(2, 50);
-delay(1000);
+  motor(1, -50);
+  motor(2, 50);
+  delay(1000);
 
-motor(1, 0);
-motor(2, 0);
-delay(1000);
+  motor(1, 0);
+  motor(2, 0);
+  delay(1000);
 }
