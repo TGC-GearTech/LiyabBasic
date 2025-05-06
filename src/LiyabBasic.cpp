3ec37a2347fa41bcc825de6daf2f12a2283bee16
TGC-GearTech <thegearconcept@gmail.com>
2025-05-06 18:00:56 +0800

    Update LiyabBasic.cpp

--- src/LiyabBasic.cpp
+++ src/LiyabBasic.cpp
@@ -5,11 +5,6 @@
 #include "Wire.h"
 #include "SPI.h"
 
-#define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 32
-
-#define OLED_RESET -1
-#define SCREEN_ADDRESS 0x3C
 extern Adafruit_SSD1306 oled;
 
 #define LOGO_HEIGHT   16
