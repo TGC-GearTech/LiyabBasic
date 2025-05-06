d27e807a4fab7ed8002f33e8f63cf7be614554c6
TGC-GearTech <thegearconcept@gmail.com>
2025-05-06 17:54:51 +0800

    Update LiyabBasic.h

--- src/LiyabBasic.h
+++ src/LiyabBasic.h
@@ -9,7 +9,15 @@
 #include "Adafruit_GFX.h"
 #include "Wire.h"
 #include "SPI.h"
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <LiyabBasic.h>
 
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 32
+
+#define OLED_RESET -1
+#define SCREEN_ADDRESS 0x3C
 Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 // Function prototypes
